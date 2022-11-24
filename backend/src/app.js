c42d33f3c0db12a9714b81cb8ab3594e1195a783
drbobby27 <envioshseq@gmail.com>
2022-11-23 21:08:06 -0500

    Define routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,10 @@ import cors from 'cors'
 import  multer  from 'multer'
 import bodyParser  from 'body-parser';
 import { errorRouter } from "./routes/error404.route.js"
+import { userRouter } from "./routes/users.route.js";
+import { roomRouter } from "./routes/room.route.js";
+import { movieRouter } from "./routes/movie.route.js";
+import { categoryRouter } from "./routes/category.route.js";
 
 const app = express()
 const upload = multer()
@@ -14,6 +18,10 @@ app.use(bodyParser.json())
 app.use(express.urlencoded({  extended: true }))
 app.use(upload.none())
 
+app.use('/api/v1/movie', movieRouter)
+app.use('/api/v1/category', categoryRouter)
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/room', roomRouter)
 app.use('*', errorRouter)
 
 export default app
