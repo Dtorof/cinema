c7eca915acdc6f275755c1c495936a0de5e51319
dtorof <dtoro.sistemas@gmail.com>
2022-11-23 21:45:33 -0500

    Rotes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,26 @@
 import express from "express"
 import morgan from "morgan"
 import cors from 'cors'
-import  multer  from 'multer'
+//import  multer  from 'multer'
 import bodyParser  from 'body-parser';
 import { errorRouter } from "./routes/error404.route.js"
-
+import { scheduleRouter } from './routes/schedule.route.js'
+import { userRouter } from './routes/users.route.js'
+import { billboardRouter } from './routes/billboard.route.js'
 const app = express()
-const upload = multer()
+//const upload = multer()
 
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.use(express.urlencoded({  extended: true }))
-app.use(upload.none())
+//app.use(upload.none())
 
+
+app.use('/api/v1/schedule', scheduleRouter)
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/billboard', billboardRouter)
 app.use('*', errorRouter)
+
 
 export default app
